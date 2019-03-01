2501ff6e26ed11a7a691269295f5bba926b3fcb6
Mohit Verma <moverm@microsoft.com>
2019-03-01 17:12:30 +0530

    Commiting AzureDevops connect solution

---
+++ src/DeveloperTools/AzureDevops.Connect/AzureDevops.Connect/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.TeamFoundation.SourceControl.WebApi;
+using Microsoft.VisualStudio.Services.Client;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureDevops.Connect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const String c_collectionUri = "https://XXX.visualstudio.com";
+            const String c_projectName = "XXX-Assets";
+            const String c_repoName = "XXXX-XXXX";
+
+            // Interactively ask the user for credentials, caching them so the user isn't constantly prompted
+            VssCredentials creds = new VssClientCredentials();
+            creds.Storage = new VssClientCredentialStorage();
+
+            // Connect to Azure DevOps Services
+            VssConnection connection = new VssConnection(new Uri(c_collectionUri), creds);
+
+            // Get a GitHttpClient to talk to the Git endpoints
+            GitHttpClient gitClient = connection.GetClient<GitHttpClient>();
+            var repo = gitClient.GetRepositoryAsync(c_projectName, c_repoName).Result;
+        }
+    }
+}
