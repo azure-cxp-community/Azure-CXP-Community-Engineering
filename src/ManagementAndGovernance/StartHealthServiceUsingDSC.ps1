8dcf84f2747a40cbcf2c651c5823808404aceed0
KrishnaG-MSFT <45988031+KrishnaG-MSFT@users.noreply.github.com>
2019-03-05 11:53:49 +0530

    Create StartHealthServiceUsingDSC.ps1

---
+++ src/ManagementAndGovernance/StartHealthServiceUsingDSC.ps1
@@ -0,0 +1,13 @@
+Configuration MMAgent
+{
+    Import-DscResource -ModuleName xPSDesiredStateConfiguration
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+
+    Node OMSnode {
+        Service OIService
+        {
+            Name = "HealthService"
+            State = "Running"
+        }
+    }
+}
